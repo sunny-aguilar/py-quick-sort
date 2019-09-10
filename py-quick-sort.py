c30ae6a8160823e4cbb5dd6f3584dbe65552fb48
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-09 22:22:08 -0700

    qs

--- py-quick-sort.py
+++ py-quick-sort.py
@@ -1,7 +1,7 @@
 # Quicksort Algorithm
 def main():
     array = [4, 2, 5, 6, 1, 3, 10, 8, 9]
-    quick_sort()
+    print(quick_sort(array))
 
 
 def quick_sort(array):
@@ -10,5 +10,7 @@ def quick_sort(array):
     else:
         pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
 
 main()
